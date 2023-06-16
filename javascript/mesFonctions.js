74b1c0af96dbb21982337717c1097903cf03dbb5
NiangPro <NiangProgrammeur@gmail.com>
2023-06-16 18:07:36 +0000

    les fonctions fleches

--- javascript/mesFonctions.js
+++ javascript/mesFonctions.js
@@ -5,3 +5,19 @@ function tableDeMultiplication(valeur)
         document.write(`${valeur} x ${i} = ${valeur * i}<br />`);
     }
 }
+
+function factoriel(nombre){
+    let produit = 1;
+    let message = "";
+    for (let i = 1; i <= nombre; i++) {
+        // message += (i == 1)? i : "x"+i;
+        if (i == 1) {
+            message = i;
+        } else {
+            message += "x"+i;
+        }
+        produit *= i;
+    }
+
+    document.write(`${nombre}! = ${message} = ${produit}`)
+}
